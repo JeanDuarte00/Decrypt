8a9d8413f4e1721d1489869f85d435f5cd5fa922
JeanDuarte00 <jean_luccas320@hotmail.com>
2017-10-22 23:22:02 -0300

    aqui chamo todas os includes: allegro e C

---
+++ lib.h
@@ -0,0 +1,27 @@
+#ifndef LIB
+#define LIB
+
+	#include <stdio.h>
+	#include <math.h>
+	#include <stdlib.h>
+	#include <time.h>
+	#include <string.h>
+	#include <stdbool.h>
+	
+	
+	#include <allegro5/allegro.h>
+	#include <allegro5/allegro_image.h>
+	#include <allegro5/allegro_audio.h>
+	#include <allegro5/allegro_acodec.h>
+	#include <allegro5/allegro_primitives.h>
+	#include <allegro5/allegro_font.h>
+	#include <allegro5/allegro_ttf.h>
+	#include <allegro5/allegro_native_dialog.h>
+
+
+	#include "lib/defines.h"
+	#include "lib/structs.h"
+
+
+
+#endif
